c27b2e417bb676421b6fe33c1e5408b526edc99c
doge <jekcatpopov@gmail.com>
2023-10-01 20:41:27 +0300

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:untitled55/view/Account/AccountScreen.dart';
 import 'package:untitled55/view/Home/HomeScreen.dart';
-import 'package:untitled55/view/Login/LoginScreen.dart';
 import 'package:untitled55/view/chat/chat_screen.dart';
 import 'package:untitled55/view/chat/groups_chats.dart';
 import 'package:untitled55/view/chats/view/chats_screen.dart';
@@ -18,8 +17,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: const HomeScreen(),
       routes: {
+        'profile': (context) => const AccountScreen(),
+        'home': (context) => const HomeScreen(),
         '/chats': (context) => const ChatsScreen(),
         '/chat': (context) => const ChatScreen(),
         '/auth': (context) => const AuthScreen(),
