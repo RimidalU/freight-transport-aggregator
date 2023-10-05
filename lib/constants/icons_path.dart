1dde09199f28b344dfbc01dd2a1843b954c7f9a3
doge <jekcatpopov@gmail.com>
2023-10-05 20:27:56 +0300

    Initial commit

---
+++ lib/constants/icons_path.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+const String filter = 'assets/icons/filter.svg';
+
+Widget filterIcon = SvgPicture.asset(filter);
--- lib/view/Settlement/SettlementScreen.dart
+++ lib/view/Settlement/SettlementScreen.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_extensions/awesome_extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:untitled55/view/global_widgets/AppbarAndNotifications.dart';
 
@@ -11,17 +12,76 @@ class SettlementScreen extends StatefulWidget {
 class _SettlementScreenState extends State<SettlementScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xff121418),
+    return Scaffold(
+      backgroundColor: const Color(0xff121418),
            body: Padding(
-             padding: EdgeInsets.symmetric(horizontal: 4.0),
+             padding: const EdgeInsets.symmetric(horizontal: 4.0),
              child: Column(
                children: [
-                 AppBarAndNotifications(),
-
+                 const AppBarAndNotifications(),
+                Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xff202329),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                 width: context.width-8,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 12.0, left: 16, right: 16),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              height: 44,
+                              width: context.width - 97,
+                              child: const SearchTextField(),
+                            ),
+                            13.widthBox,
+                            Container(
+                              height: 44,
+                              width: 44,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(12),
+                                color: const Color(0xffFCFCFC).withOpacity(0.08)
+                              ),
+                            )
+                          ],
+                        ),
+                        12.heightBox,
+                        Text('')
+                      ],
+                    ),
+                  ),
+                ),
                ],
              ),
            ),
+    );
+  }
+}
+
+
+
+class SearchTextField extends StatelessWidget {
+  const SearchTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderSide: BorderSide(color: const Color(0xFCFCFC).withOpacity(0.08)),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: const Color(0xFCFCFC).withOpacity(0.08)),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: const Color(0xFCFCFC).withOpacity(0.08)),
+        ),
+        prefixIcon: const Icon(Icons.search),
+        hintText: 'Search',
+      ),
     );
   }
 }
